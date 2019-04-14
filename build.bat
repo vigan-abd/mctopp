6ff178e768a9e04f8ae8fae2135fd9589485aa70
Vigan Abdurrahmani <vigan.abd@gmail.com>
2019-04-14 16:27:29 -0700

    Bat file adjusted

--- build.bat
+++ build.bat
@@ -2,4 +2,5 @@
 
 IF EXIST program.exe DEL /F program.exe
 CALL dotnet publish -c App -r win10-x64
-CALL mklink ".\\bin\\App\\netcoreapp2.2\\ubuntu.16.10-x64\\MCTOPP" ".\\program"
+CALL cd ".\\bin\\App\\netcoreapp2.2\\win10-x64"
+ECHO "Program is stored in .\\bin\\App\\netcoreapp2.2\\win10-x64\\MCTOPP.exe"
